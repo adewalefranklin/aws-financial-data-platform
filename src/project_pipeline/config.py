a6379aee955feef6b02505ea5618039697564287
Adewale Franklin Hassan <122757918+adewalefranklin@users.noreply.github.com>
2026-07-05 10:13:15 +0200

    Configuration setup implemented

---
+++ src/project_pipeline/config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv 
+from src.project_pipeline.exceptions import ConfigError
+
+load_dotenv()
+
+class Config:
+    def __init__(self):
+        self.finnhub_api_key = os.getenv("FINNHUB_API_KEY")
+        self.finnhub_base_url = os.getenv("FINNHUB_BASE_URL")
+        if not self.finnhub_api_key:
+            raise ConfigError("FINNHUB_API_KEY is not set in the environment variables.")
+        if not self.finnhub_base_url:
+            raise ConfigError("FINNHUB_BASE_URL is not set in the environment variables.")
+        
---
+++ src/project_pipeline/exceptions.py
@@ -0,0 +1,10 @@
+class PipelineError(Exception):
+    pass
+class ExtractError(PipelineError):
+    pass 
+class TransformError(PipelineError):
+    pass
+class LoadError(PipelineError):
+    pass
+class ConfigError(PipelineError):
+    pass
