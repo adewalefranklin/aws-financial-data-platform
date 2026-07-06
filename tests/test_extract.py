4728eaf8e199a283b87ddbf86b1eec0378db92d2
Adewale Franklin Hassan <adewale.franklin@gmail.com>
2026-07-06 10:19:21 +0200

    Test Extract Implemented

---
+++ tests/test_extract.py
@@ -0,0 +1,28 @@
+from src.project_pipeline.extract import Extract
+
+def test_extract_success(mocker, monkeypatch):
+    monkeypatch.setenv("FINNHUB_API_KEY", "fake_key")
+    monkeypatch.setenv("FINNHUB_BASE_URL", "http://fakeurl.com")
+
+    fake_response = mocker.Mock()
+    fake_response.raise_for_status.return_value = None
+    fake_response.json.return_value = {"data": "test_data"}
+
+    mock_get = mocker.patch(
+        "src.project_pipeline.extract.requests.get",
+        return_value=fake_response
+    )
+
+    extractor = Extract()
+    result = extractor.fetch_stock_data("AAPL")
+
+    assert result == {"data": "test_data"}
+
+    mock_get.assert_called_once_with(
+        "http://fakeurl.com/quote",
+        params={
+            "symbol": "AAPL",
+            "token": "fake_key"
+        },
+        timeout=30
+    )
