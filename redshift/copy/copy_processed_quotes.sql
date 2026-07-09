c3cd9f4b83e548c758d60d6f4084cce50ff55427
Adewale Franklin Hassan <122757918+adewalefranklin@users.noreply.github.com>
2026-07-09 12:45:44 +0200

    kinesis Streaming implemented as well as  Bronze and Gold layer

--- redshift/copy/copy_processed_quotes.sql
+++ redshift/copy/copy_processed_quotes.sql
@@ -1,4 +1,4 @@
 COPY bronze.finnhub_quotes_processed
 FROM 's3://aws-data-engineering-platform/processed/finnhub/quotes/'
-IAM_ROLE 'my-redshift-iam-role-arn>'
+IAM_ROLE 'my-redshift-iam-role-arn'
 FORMAT AS PARQUET;
--- redshift/copy/copy_streaming_trades.sql
+++ redshift/copy/copy_streaming_trades.sql
@@ -1,4 +1,4 @@
 COPY bronze.finnhub_streaming_trades
 FROM 's3://aws-data-engineering-platform/processed/finnhub/streaming_trades/'
-IAM_ROLE 'my-redshift-iam-role-arn>'
+IAM_ROLE 'my-redshift-iam-role-arn'
 FORMAT AS PARQUET;
